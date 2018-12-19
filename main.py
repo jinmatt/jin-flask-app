264b665a48654b04a9c39aecfa42aa1f8d545148
Jinsu Mathew <jinsu.mails@gmail.com>
2018-12-19 15:43:25 -0800

    health endpoint

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    return "Hello, flask!"
+def index():
+    return "hello, flask!"
+
+
+@app.route("/health")
+def health():
+    return "healthy"
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0')
